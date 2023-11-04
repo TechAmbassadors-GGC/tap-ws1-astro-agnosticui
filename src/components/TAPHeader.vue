16475d10ee24ca360d457e704565e1efab9e3963
cengique <cengique@users.sf.net>
2023-11-04 17:19:27 -0400

    new header menu

---
+++ src/components/TAPHeader.vue
@@ -0,0 +1,26 @@
+<script setup>
+// Import AgnosticUI global common & component CSS
+import "agnostic-vue/dist/common.min.css";
+import "agnostic-vue/dist/index.css";
+import { Header, HeaderNav, HeaderNavItem } from "agnostic-vue";
+</script>
+<template>  
+  <Header isHeaderContentEnd>
+    <template v-slot:logoleft>
+      <a class="flex-fill" href="https://web.dev/" >web.dev</a>
+    </template>
+    <template v-slot:headernav>
+      <HeaderNav css="header-mbe16">
+        <HeaderNavItem>
+          <a href="https://css-tricks.com/">CSS-Tricks</a>
+        </HeaderNavItem>
+        <HeaderNavItem>
+          <a href="https://developer.mozilla.org/en-US/">MDN</a>
+        </HeaderNavItem>
+      </HeaderNav>
+    </template>
+    <template v-slot:logoright>
+      <a href="https://www.freecodecamp.org/">freeCodeCamp</a>
+    </template>
+  </Header> 
+</template>
