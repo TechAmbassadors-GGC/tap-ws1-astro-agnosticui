d1e7835da3ab31831aa835f526932dca31de20b9
Chidiebube <ebubeokebalama@gmail.com>
2024-02-15 16:17:41 -0500

    Created crumb components for each page
    (I want there to be a way to find out what page or URL the user is on and use that info to create an array of strings that can be used to set up the URL argument for the Breadcrumb module.)

---
+++ src/components/BlogCrumbs.vue
@@ -0,0 +1,26 @@
+<script setup>
+
+// Import of AugnosticUI global common and its CSS
+import "agnostic-vue/dist/common.min.css";
+import "agnostic-vue/dist/index.css";
+import Breadcrumb from "./vue/Breadcrumb.vue";
+
+
+const trailOfTAPSite = [
+    {
+      label: "TAP",
+      url: "/",
+    },
+    {
+      label: "Blog",
+    }
+];
+
+</script>
+
+<template>
+    <h2>Breadcrumbs</h2>
+    <div class="mbs24 mbe16">
+        <Breadcrumb type="slash" :routes="trailOfTAPSite" />
+    </div>
+</template>
--- src/components/ProjectCrumbs.vue
+++ src/components/ProjectCrumbs.vue
@@ -6,44 +6,29 @@ import "agnostic-vue/dist/common.min.css";
 import "agnostic-vue/dist/index.css";
 import Breadcrumb from "./vue/Breadcrumb.vue";
 // import Vue from 'vue'
-// import Router from 'vue-router'
+import { useRouter } from 'vue-router'
 
 // Vue.use(Router);
 
-// const Home = { template: '<div>TAP</div>' }
-// const Projects = { template: '<div>Projects</div>' }
+// const home = { template: '<div>TAP</div>' }
+// const projects = { template: '<div>Projects</div>' }
+// const posts = { template: '<div>Posts</div>' }
 
-// const router = new Router({
-//     routes:[
-//         {
-//             path: '/',
-//             component: Home
-//         },
-//         {
-//             path: '/projects',
-//             component: Projects
-//         }
-//     ]
-// })
 
-// export default {
 //     goBack() {
 //         window.history.length > 1 ? this.$router.go(-1) : this.$router.push('/')
 //     }
-// }
-var current = '';
-
 
 
 const trailOfTAPSite = [
     {
-        label: "TAP",
-        url: "/",
+      label: "TAP",
+      url: "/",
     },
     {
-        label: "Projects",
-        url: "/projects",
-    },
+      label: "Projects",
+      url: "",
+    }
 ];
 
 </script>
