a57b582ba1d9e9a0433093c71fd708fb52dc813c
Chidiebube <ebubeokebalama@gmail.com>
2024-02-15 16:28:33 -0500

    Renamed the post pages crumb component

--- src/components/PostCrumbs.vue
+++ src/components/PostCrumbs.vue
@@ -6,6 +6,17 @@ import "agnostic-vue/dist/index.css";
 import Breadcrumb from "./vue/Breadcrumb.vue";
 
 
+// Vue.use(Router);
+
+// const home = { template: '<div>TAP</div>' }
+// const projects = { template: '<div>Projects</div>' }
+// const posts = { template: '<div>Posts</div>' }
+
+
+//     goBack() {
+//         window.history.length > 1 ? this.$router.go(-1) : this.$router.push('/')
+//     }
+
 const trailOfTAPSite = [
     {
       label: "TAP",
