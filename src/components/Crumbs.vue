d9f15d99446c492c9ed55537bc87537e52006960
Chidiebube <ebubeokebalama@gmail.com>
2024-02-15 23:54:16 -0500

    Cleaned up the old astro bread crumbs and made the vue 3 bread crumbs work on projects page using props

--- src/components/Crumbs.vue
+++ src/components/Crumbs.vue
@@ -1,4 +1,3 @@
-
 <script setup>
 
 // Import of AugnosticUI global common and its CSS
@@ -6,22 +5,41 @@ import "agnostic-vue/dist/common.min.css";
 import "agnostic-vue/dist/index.css";
 import Breadcrumb from "./vue/Breadcrumb.vue";
 
+//set up of the props for seperate pages
+
+// create a prop that can be edited on each of the pages this component is used on and then use the prop as a variable to be entered into the Breadcrumb url module parameter
+
+const pageProp = defineProps(
+  {
+    pageLabel: String,
+    pageUrl: String
+  }
+);
+
+
+
+//bread crumb set up
+
 const trailOfTAPSite = [
     {
-      label: "TAP",
-      url: "/",
+      label: `${pageProp.pageLabel.split(",")[0]}`,
+      url: `${pageProp.pageUrl.split(",")[0]}`,
     },
     {
-      label: "Projects",
-      url: "",
+      label: `${pageProp.pageLabel.split(",")[1]}`,
     }
 ];
 
 </script>
 
+<style>
+/* #crumb{ */
+  /* background-color: rgb(0, 0, 157); */
+/* } */
+</style>
+
 <template>
-    <h2>Breadcrumbs</h2>
-    <div class="mbs24 mbe16">
-        <Breadcrumb type="slash" :routes="trailOfTAPSite" />
+    <div id="crumb" class="mbs24 mbe16">
+        <h4><Breadcrumb type="slash" :routes="trailOfTAPSite" /></h4>
     </div>
 </template>
