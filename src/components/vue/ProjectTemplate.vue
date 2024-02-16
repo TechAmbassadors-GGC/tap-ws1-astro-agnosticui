f41e69aa71adec8743cb2cce933f39cf1de81823
Farce4wrd <74463978+Farce4wrd@users.noreply.github.com>
2024-02-15 22:48:21 -0500

    created project template page

---
+++ src/components/vue/ProjectTemplate.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { computed, ref } from "vue";
+// Components CSS
+import "agnostic-vue/dist/index.css";
+import "agnostic-vue/dist/common.min.css";
+import { Input, Card, Select } from "agnostic-vue";
+
+
+// load blog content: news, etc.
+import { getCollection } from 'astro:content';
+const projects = await getCollection('projects');  //list of projects
+
+</script>
+<template>
+    <section class="mbe40">
+        <label>Enter Project Name:</label>
+        <input>
+
+
+    </section>
+
+</template>
+  
+  
+  
+<style scoped></style>
+  
