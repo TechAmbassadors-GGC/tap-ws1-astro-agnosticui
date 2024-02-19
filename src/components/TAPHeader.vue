7efbb69b223ba0b9038e56533b8b08615750bd3e
itskelechi <149823828+itskelechi@users.noreply.github.com>
2024-02-18 21:29:47 -0500

    changed the display from grid to list and pulled the header out of main

--- src/components/TAPHeader.vue
+++ src/components/TAPHeader.vue
@@ -7,22 +7,26 @@ import { Header, HeaderNav, HeaderNavItem, Menu } from "agnostic-vue";
 <template>  
   <Header isHeaderContentEnd>
     <template v-slot:logoleft>
+      
       <a class="flex-fill" href="/" >TAP</a>
+
     </template>
     <template v-slot:headernav>
       <HeaderNav css="header-mbe16">
         <HeaderNavItem>
-          <a href="projects">Outreach projects & templates</a>
+         
+          <a href="projects">Search Outreach Projects:</a>
         </HeaderNavItem>
         <HeaderNavItem>
           <a href="">Join the discussion</a>
         </HeaderNavItem>
+      
         <HeaderNavItem>
-          <Menu id="menu-about" menuTitle="About" :disabledItems="['menuitem-1', 'menuitem-2', 'menuitem-people']" :isBordered="true" type="simple" >
-            <template #menuitem-1>History</template>
-            <template #menuitem-people>People</template>
+          <Menu id="menu-Home" menuTitle="Home" :disabledItems="['menuitem-1', 'menuitem-2', 'menuitem-people']" :isBordered="true" type="simple" >
+            <template #menuitem-1>About</template>
+            <template #menuitem-people>Projects</template>
             <template #menuitem-2>Awards</template>
-            <template #menuitem-3><a href="">Join</a></template>            
+            <template #menuitem-3><a href="">Apply</a></template>            
             <template #icon>&#9662;</template>
           </Menu>
         </HeaderNavItem>
