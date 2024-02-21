f30a42c6280a63a0d89036e8572aa1e7d30f502d
Farce4wrd <74463978+Farce4wrd@users.noreply.github.com>
2024-02-21 09:08:38 -0500

    changes to ProjectTemplate

--- src/components/vue/ProjectTemplate.vue
+++ src/components/vue/ProjectTemplate.vue
@@ -8,13 +8,22 @@ import { Input, Card, Select } from "agnostic-vue";
 
 // load blog content: news, etc.
 import { getCollection } from 'astro:content';
-const projects = await getCollection('projects');  //list of projects
+// const projects = await getCollection('projects');  //list of projects
 
 </script>
 <template>
     <section class="mbe40">
         <label>Enter Project Name:</label>
         <input>
+        <label>Enter Project Description</label>
+        <input>
+
+        <!--To get the students from dropdown menu or to manually enter the students by email-->
+        <label>Enter Students name</label>
+        <!--Autocomplete input form to show students available-->
+        <input>
+
+        
 
 
     </section>
