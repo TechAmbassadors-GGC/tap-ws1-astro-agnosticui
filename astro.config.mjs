dc84340ad8a671cf57f0fc0511d8388f26c5b4db
Cengiz Gunay <cengique@users.sf.net>
2024-05-17 16:51:11 -0400

    Added google analytics tag through partytown integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,9 @@ import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
 import agnosticAstro from 'agnosticui-astro';
 import mdx from '@astrojs/mdx';
+import partytown from "@astrojs/partytown";
 
+// https://astro.build/config
 export default defineConfig({
-	integrations: [vue(), agnosticAstro(), mdx()],
+	integrations: [vue(), agnosticAstro(), mdx(), partytown()],
 });
