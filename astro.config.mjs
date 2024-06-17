0fb469597987f1a24ebaed5f4780f8629ebc283a
trungddao <trungdaoduc162@gmail.com>
2024-06-16 23:51:13 -0400

    New branch, display alt for logo, change heading back to ##

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
 import agnosticAstro from 'agnosticui-astro';
+import mdx from '@astrojs/mdx';
+
 
 export default defineConfig({
-	integrations: [vue(), agnosticAstro()],
+	integrations: [vue(), agnosticAstro(),mdx()],
 });
