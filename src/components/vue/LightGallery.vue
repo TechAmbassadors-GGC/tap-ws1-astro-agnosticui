d21719bfab35ee425d754945c289f65d1ec1e150
trungddao <trungdaoduc162@gmail.com>
2024-06-26 14:58:17 -0400

    testing with LightGallery

---
+++ src/components/vue/LightGallery.vue
@@ -0,0 +1,34 @@
+<template>
+    <div ref="lightGallery">
+      <slot></slot>
+    </div>
+  </template>
+  
+  <script>
+  
+  import lightGallery from 'lightgallery';
+  import lgThumbnail from 'lightgallery/plugins/thumbnail';
+  import lgZoom from 'lightgallery/plugins/zoom';
+  
+  export default {
+    name: 'LightGallery',
+    data() {
+      return {
+        plugins: [lgThumbnail, lgZoom],
+      };
+    },
+    mounted() {
+      lightGallery(this.$refs.lightGallery, {
+        speed: 500,
+        plugins: this.plugins,
+      });
+    },
+  };
+  </script>
+  
+  <style scoped>
+  @import 'lightgallery/css/lightgallery.css';
+  @import 'lightgallery/css/lg-thumbnail.css';
+  @import 'lightgallery/css/lg-zoom.css';
+  </style>
+  
