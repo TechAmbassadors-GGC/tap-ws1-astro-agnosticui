0bbc9ace09d7b99fc00a31013bbf549729e3b677
trungddao <trungdaoduc162@gmail.com>
2024-11-07 20:52:46 -0500

    support empty student

--- src/components/StudentCard.vue
+++ src/components/StudentCard.vue
@@ -11,7 +11,7 @@ const studentProp = defineProps({
         item: Object
     });
 
-const imageSrc = computed(() => studentProp.item.data.image || 'path/to/default-image.jpg');
+const imageSrc = computed(() => studentProp.item.data.image || 'src/content/students/empty.png');
 const studentName = computed(() => studentProp.item.data.name || 'Unknown Student');
 
 
