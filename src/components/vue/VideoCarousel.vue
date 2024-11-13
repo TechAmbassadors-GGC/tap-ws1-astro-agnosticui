5bb8c08a5a0196997c0fe8082bb22621337dacf8
itskelechi <149823828+itskelechi@users.noreply.github.com>
2024-11-12 20:47:18 -0500

    debugs for Dr. G

--- src/components/vue/VideoCarousel.vue
+++ src/components/vue/VideoCarousel.vue
@@ -1,49 +1,56 @@
----
-import './src/styles/global.css';
+<script setup lang="ts">
+
+import '/src/styles/global.css';
 
 interface ClipProps {
   src: string;
   caption?: string;
 }
 
-interface Props {
+interface Props{
   videos: ClipProps[];
   showIframe: boolean;
 }
 
-const { videos, showIframe } = Astro.props;
+const props = defineProps<Props>(); 
+
+const { videos, showIframe } = props;
 
 let cIndex = 0;
 
 function next() {
   cIndex = cIndex === videos.length - 1 ? 0 : cIndex + 1;
+  console.log("CIndex:");
+  console.log(cIndex);
 }
 
 function prev() {
   cIndex = cIndex === 0 ? videos.length - 1 : cIndex - 1;
 } 
----
+</script>
+
+<template>
 
 <section>
-  {showIframe && (
-    <div>
+    <div v-if="showIframe">
       <iframe
         class="video-section"
-        src={`https://www.youtube.com/embed/${videos[cIndex].src}`}
+        :src="`https://www.youtube.com/embed/${videos[cIndex].src}`"
         title="YouTube video player"
         frameborder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         referrerpolicy="strict-origin-when-cross-origin"
         allowfullscreen
       ></iframe>
-      {videos[cIndex].caption && <p>{videos[cIndex].caption}</p>}
-        <a onclick={prev} class="prev">&#10094;</a>
-        <a onclick={next} class="next">&#10095;</a>
+      <p v-if="videos[cIndex].caption">{{videos[cIndex].caption}}</p>
+        <a :onclick="prev()" class="prev">&#10094;</a>
+        <span @click="next()" class="next">&#10095;</span>
     </div>
-  )}
 </section>
 
-<style>
+</template>
+
+<style scoped>
    .video-section {
     width: 100%;
     max-width: 800px;
